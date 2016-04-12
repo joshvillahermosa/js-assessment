ccfc17f72f56bde37d73853c35899de6fce35d14
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2016-04-12 18:32:26 -0400

    [async.js] Fixed first async test

--- app/async.js
+++ app/async.js
@@ -3,6 +3,13 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.asyncAnswers = {
   async : function(value) {
 
+    return new Promise(function (resolve, reject) {
+      try {
+        resolve(value);
+      } catch(e) {
+        reject(e);
+      }
+    });
   },
 
   manipulateRemoteData : function(url) {
