9158d05f4888b8a24ea497fa50d8c90459237aa7
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2016-04-12 19:19:52 -0400

    [objects.js] Finished first objects test with simple answer

--- app/objects.js
+++ app/objects.js
@@ -3,6 +3,10 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
 
+  	// Simplified Answer
+  	obj.sayIt = fn;
+
+  	return obj.sayIt();
   },
 
   alterObjects : function(constructor, greeting) {
