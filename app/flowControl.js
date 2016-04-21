54aac2694333a71229db8559b7225194b3eb7186
joshvillahermosa <joshvillahermosa@gmail.com>
2016-04-21 12:08:15 -0400

    [flowControl.js] Finished set of tests

--- app/flowControl.js
+++ app/flowControl.js
@@ -10,6 +10,30 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
+    var threeAndFiveTeigger = false;
+
+    if (num === undefined) {
+      return false;
+    }
+
+    if (typeof(num) !== 'number') {
+      return false;
+    }
+
+    if (((num % 3) === 0) && ((num % 5) === 0)) {
+      return 'fizzbuzz';
+      threeAndFiveTeigger = true;
+    }
+    
+    if (((num % 3) === 0) && (threeAndFiveTeigger === false)) {
+      return 'fizz';
+    }
+
+    if (((num % 5) === 0) &&  (threeAndFiveTeigger === false)) {
+      return 'buzz';
+    }
+
+    return num;
 
   }
 };
